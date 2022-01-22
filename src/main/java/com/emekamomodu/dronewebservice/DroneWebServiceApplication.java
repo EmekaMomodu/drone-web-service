26f8a303c59421635649f82afb5e108e55fa5f39
Chukwuemeka Momodu <momodu.ceo@gmail.com>
2022-01-22 15:42:51 +0100

    reformat

--- src/main/java/com/emekamomodu/dronewebservice/DroneWebServiceApplication.java
+++ src/main/java/com/emekamomodu/dronewebservice/DroneWebServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DroneWebServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DroneWebServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DroneWebServiceApplication.class, args);
+    }
 
 }
