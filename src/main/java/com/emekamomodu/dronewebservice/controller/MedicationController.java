ba4d8632c35346aabd53184fca0e6118ef46133b
Chukwuemeka Momodu <momodu.ceo@gmail.com>
2022-01-22 15:46:48 +0100

    made image request part optional

--- src/main/java/com/emekamomodu/dronewebservice/controller/MedicationController.java
+++ src/main/java/com/emekamomodu/dronewebservice/controller/MedicationController.java
@@ -7,8 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * @author CMOMODU
@@ -23,7 +28,7 @@ public class MedicationController {
     private MedicationService medicationService;
 
     @PostMapping(value = "/register", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<Response> registerMedication(@RequestPart("medication") MedicationModel medicationModel, @RequestPart("image") MultipartFile medicationImage) {
+    public ResponseEntity<Response> registerMedication(@RequestPart("medication") MedicationModel medicationModel, @RequestPart(name = "image", required = false) MultipartFile medicationImage) throws IOException {
         return ResponseEntity.status(HttpStatus.CREATED).body(medicationService.registerMedication(medicationModel, medicationImage));
     }
 
