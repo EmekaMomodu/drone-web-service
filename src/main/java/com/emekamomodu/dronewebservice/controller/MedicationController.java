3b6fe7bd5fc985d5f1828caa069d4c8aca6139a2
Chukwuemeka Momodu <momodu.ceo@gmail.com>
2022-01-23 18:22:46 +0100

    getAllMedications controller added

--- src/main/java/com/emekamomodu/dronewebservice/controller/MedicationController.java
+++ src/main/java/com/emekamomodu/dronewebservice/controller/MedicationController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -30,6 +27,11 @@ public class MedicationController {
     @PostMapping(value = "/register", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<Response> registerMedication(@RequestPart("medication") MedicationModel medicationModel, @RequestPart(name = "image", required = false) MultipartFile medicationImage) throws IOException {
         return ResponseEntity.status(HttpStatus.CREATED).body(medicationService.registerMedication(medicationModel, medicationImage));
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<Response> getAllMedication() {
+        return ResponseEntity.ok(medicationService.getAllMedication());
     }
 
 }
